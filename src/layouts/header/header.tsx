f61afffa7c99f009332e7d45ed1779a063b0c3f3
kckcjs s smxmnd d@(@(? <118021544+AAkismetKLASNI@users.noreply.github.com>
2024-12-23 12:04:34 +0500

    chore/style: незначительное изменение в стилях

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className='py-4 flex justify-between items-center mb-20 lg:mb-40 xl:mb-60'>
+    <header className='py-4 flex justify-between items-center mb-20 lg:mb-40'>
       <Link href='/'>
         <Image src='/logo.svg' alt='logo' height='45' width='45' />
       </Link>
